0eacf3a5d8d64a5bb43f7ef3e18964e065131ad5
BRunknown <asafe.ferr@gmail.com>
2026-03-07 22:53:19 -0300

    <feat>: estrutura inicial app

--- parte_1/pokeapi_etl/main.py
+++ parte_1/pokeapi_etl/main.py
@@ -1,5 +1,16 @@
-def hello_word():
-    return "hello_word!"
+from api.api_handler import busca_lista_pokemons, busca_pokemon
+from storage.storage import OperadorArmazenamento
 
 
-print(hello_word())
+def pokedex():
+
+    lista_pokemons = busca_lista_pokemons().json().get("results")
+
+    for poke in lista_pokemons:
+        pokemon_info = busca_pokemon(poke.get("url"))
+        OperadorArmazenamento.registra_dados_brutos(pokemon_info)
+        OperadorArmazenamento.registra_dados_bd(pokemon_info)
+
+
+if __name__ == "__main__":
+    pokedex()

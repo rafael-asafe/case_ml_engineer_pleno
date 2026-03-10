13dab7382bc23cbd0d07f5f266f4cf6e19b43fc1
BRunknown <asafe.ferr@gmail.com>
2026-03-10 02:06:17 -0300

    fix: write pokemon_ability tab, cast is_hidden as bool

--- parte_1/pokeapi_etl/database/database.py
+++ parte_1/pokeapi_etl/database/database.py
@@ -16,7 +16,14 @@ def exporta_para_parquet(nome_tabela: str,destino_tabela: str,engine:Engine = en
     try:
         query = f"SELECT * FROM {nome_tabela}"
         df = pl.read_database(query=query, connection=engine)
-        df.write_parquet(destino_tabela, compression="snappy")
+
+        # TODO resolve usando um postgres - sqlite não tem suporte a bool
+        if nome_tabela == 'pokemon_ability':
+            df_ajustado = df.with_columns(pl.col("is_hidden").cast(pl.Boolean))
+            df_ajustado.write_parquet(destino_tabela,compression="snappy")
+            return 
+        
+        df.write_parquet(destino_tabela,compression="snappy")
     
     except Exception as e:
         logger.error(f"erro ao salvar tabela: {nome_tabela} em parquet,\n erro: {e}")

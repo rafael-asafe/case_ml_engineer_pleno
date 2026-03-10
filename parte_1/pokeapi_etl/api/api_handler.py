4f7b82db749cd41c10ab778cb56d74ffedb17f5f
BRunknown <asafe.ferr@gmail.com>
2026-03-09 22:14:19 -0300

    refactor & feat: add paginacao, atualizando func para async

--- parte_1/pokeapi_etl/api/api_handler.py
+++ parte_1/pokeapi_etl/api/api_handler.py
@@ -1,43 +1,45 @@
-import json
+from asyncio import gather
+from itertools import chain
 
-import httpx
+from httpx import AsyncClient, Response
 
-from api.client import client
-from utils.logger import logger
-
-
-def trata_erro_http(func):
-    def wrapper(*args, **kwargs):
-        try:
-            logger.debug(f"=== Iniciando {func.__name__}")
-            response = func(*args, **kwargs)
-            logger.debug(f"Status HTTP: {response.status_code}")
-            response.raise_for_status()
-            return response
-        except httpx.HTTPStatusError as e:
-            logger.error(f"✗ Erro HTTP {e.response.status_code}: {e} ")
-            return False
-        except httpx.RequestError as e:
-            logger.error(f"✗ Erro de conexão: {e} ")
-            return False
-        except json.JSONDecodeError as e:
-            logger.error(f"✗ Erro no formato JSON: {e} ")
-            return False
-        except IOError as e:
-            logger.error(f"✗ Erro de I/O: {e} ")
-            return False
-        except Exception as e:
-            logger.exception(f"✗ Erro inesperado: {e} ")
-            return False
-
-    return wrapper
+from api.client import async_client, trata_erro_http_async
+from utils.settings import Settings
 
 
-@trata_erro_http
-def busca_pokemon(pokemon_url, client=client):
-    return client.get(pokemon_url)
+@trata_erro_http_async
+async def busca_pagina(
+    url: str, limit: int, offset: int, async_client: AsyncClient = async_client
+) -> Response:
+    params = {'offset': offset, 'limit': limit}
+    response = await async_client.get(url, params=params)
+    return response
 
 
-@trata_erro_http
-def busca_lista_pokemons(client=client):
-    return client.get("/pokemon")
+async def paginacao(url: int, async_client: AsyncClient = async_client) -> list:
+    paginas = []
+    retorno = await async_client.get(url)
+    paginas += retorno.json()['results']
+    total = retorno.json().get('count')
+
+    retornos = await gather(*[
+        busca_pagina(url, Settings().LIMIT_OFFSET, offset)
+        for offset in range(Settings().LIMIT_OFFSET, total, Settings().LIMIT_OFFSET)
+    ])
+
+    paginas.extend(chain.from_iterable(r.json()['results'] for r in retornos))
+
+    return paginas
+
+
+async def busca_lista_pokemons() -> list[Response]:
+    retornos = await paginacao('/pokemon')
+    return retornos
+
+
+@trata_erro_http_async
+async def busca_pokemon(
+    pokemon_url: str, async_client: AsyncClient = async_client
+) -> Response:
+    response = await async_client.get(pokemon_url)
+    return response

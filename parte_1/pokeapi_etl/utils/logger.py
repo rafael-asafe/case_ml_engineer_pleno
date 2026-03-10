31c595370d5a1e8b9b57f20584bc223061e8383f
BRunknown <asafe.ferr@gmail.com>
2026-03-09 22:01:35 -0300

    <feat>: add execution id e ajustes config log level

--- parte_1/pokeapi_etl/utils/logger.py
+++ parte_1/pokeapi_etl/utils/logger.py
@@ -1,10 +1,12 @@
 import logging
 import sys
+import uuid
 
 from utils.settings import Settings
 
+execution_id = str(uuid.uuid4())
 
-def setup_logger(log_to_console=True):
+def setup_logger(log_to_console: bool = True) -> logging.Logger:
     """Configuração do logger"""
 
     # inicializa o logger
@@ -12,14 +14,13 @@ def setup_logger(log_to_console=True):
     logger.setLevel(Settings().LOG_LEVEL)
 
     # específica o formato da mensagem
-    formatter = logging.Formatter(
-        "[%(asctime)s] %(levelname)-8s %(message)s", datefmt="%H:%M:%S"
+    formatter = logging.Formatter(f"[%(asctime)s] %(levelname)-8s %(message)s exec_id:[{execution_id}]", datefmt="%H:%M:%S"
     )
 
     # adiciona um handler para mostrar as informações no stout, caso necessário
     if log_to_console:
         console_handler = logging.StreamHandler(sys.stdout)
-        console_handler.setLevel(logging.INFO)  # Mostra INFO e acima no terminal
+        console_handler.setLevel(Settings().LOG_LEVEL)  # Mostra INFO e acima no terminal
         console_handler.setFormatter(formatter)
         logger.addHandler(console_handler)
 
